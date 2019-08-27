ed9564fa761b57c3e74d377b6e11b4cfaf3d9cc9
Abylay Murzashev <Murzashev>
2019-08-27 21:01:22 +0600

    injected adsense script

--- resources/js/src/containers/home/index.js
+++ resources/js/src/containers/home/index.js
@@ -1,8 +1,23 @@
+/* eslint-disable react/no-danger */
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { css } from 'aphrodite';
 import styles from './styles';
 import { Cities, Universities } from './components';
+
+const injectScript = () => (
+  <div
+    dangerouslySetInnerHTML={{
+      __html: `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <script>
+            (adsbygoogle = window.adsbygoogle || []).push({
+                google_ad_client: "ca-pub-6009075208489133",
+                enable_page_level_ads: true
+            });
+      </script>`
+    }}
+  />
+);
 
 const Home = () => (
   <section className={`section_padding_std ${css(styles.homeWrap)} center_down`}>
@@ -11,6 +26,7 @@ const Home = () => (
     </Helmet>
     <Cities />
     <Universities />
+    {injectScript()}
   </section>
 );
 export default Home;
